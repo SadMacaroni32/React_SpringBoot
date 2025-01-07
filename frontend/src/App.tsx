18b09b2fa793c7dbc188e7cb76e6819c746ce445
JE <flash.staffsuport@gmail.com>
2025-01-07 09:48:40 +0800

    Initial Fucking Upload

---
+++ frontend/src/App.tsx
@@ -0,0 +1,16 @@
+import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'; // Import BrowserRouter, Routes, Route
+
+import { MainPage } from './pages'; // Assuming MainPage is in './pages'
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<MainPage />} /> {/* Define path and element */}
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
